1bd0b5e09a2e01abba385778ab0ef22634675be6
llllxf <869907853@qq.com>
2019-08-29 20:33:16 +0800

    833

--- backend/model/kb_prepare/neo4j_prepare.py
+++ backend/model/kb_prepare/neo4j_prepare.py
@@ -4,9 +4,9 @@ import os
 #import pandas as pd
 import  py2neo
 from py2neo import Node, Graph, Relationship
-#from pandas import DataFrame
-#import numpy as np
-#import jieba
+from pandas import DataFrame
+import numpy as np
+import jieba
 import xlrd
 class Neo4jPrepare(object):
 
